5e0d9bd77394f38d0018f73f2b0b0190eb9a0d1b
Vladislav <vladislav.vladvlad2015@yandex.ru>
2023-07-24 13:23:44 +0300

    Coordinates added

--- src/Coordinates.java
+++ src/Coordinates.java
@@ -1,3 +1,19 @@
 public class Coordinates {
-    public int x,y;
+    private int x,y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+    public int getX() {
+        return x;
+    }
+    public void setCoordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
